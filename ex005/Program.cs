8b043c7382fd3439246e97ca8bca9e3c807d234f
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-12 21:48:13 +0300

    add ex005 project

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("write user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("yeah, it`s Masha!");
+}
+else
+{
+    Console.Write("hello, ");
+    Console.WriteLine(username);
+}
