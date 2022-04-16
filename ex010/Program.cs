0a8af0bb521a5795663a496ca1ef464efb2a3bf0
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-16 13:13:13 +0300

    add ex008, ex009, ex010, ex011 projects

---
+++ ex010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 15, 52, 73, 94, 25, 36, 73, 84, 19 };
+
+int n = array.Length;
+int find = 73;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)   
+    {
+        Console.WriteLine(index);
+        break; // чтобы найти индекс только первого числа
+    }
+    //index = index + 1;
+    index++;
+}
