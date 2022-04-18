e94a5f02eb15f5c70b5dd184f32056d889aeecee
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-18 22:22:50 +0300

    add ex012, ex013 projects

---
+++ ex012/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 7;
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
---
+++ ex013/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 100;
+int numberB = 5;
+
+if(numberA % numberB == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    int remains = numberA / numberB;
+    Console.WriteLine("no, remains " + remains);
+}
